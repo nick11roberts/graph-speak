a56e3c8d5280d18097f48ed800d30c8c1539df7e
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2015-06-23 18:22:04 -0700

    Parser works.

--- brain/src/main/java/io/nick11roberts/github/brain/TrainingExampleManager.java
+++ brain/src/main/java/io/nick11roberts/github/brain/TrainingExampleManager.java
@@ -1,6 +1,7 @@
 package io.nick11roberts.github.brain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,8 +11,17 @@ public class TrainingExampleManager {
     public TrainingExampleManager(){}
 
     private List<List<String>> parsedText = new ArrayList<List<String>>();
+    private final Boolean DEBUG = true;
 
     public List<List<String>> parseText(String text){
+        String[] textSentence = text.split("(?<=[.!?])\\s*"); //Splits text into sentences
+        for (int i = 0; i <= textSentence.length-1; i++) {
+            parsedText.add(Arrays.asList(textSentence[i].toLowerCase().replaceAll("[^a-z ]", "").split(" ")));
+        }
+
+        if (DEBUG) {
+            System.out.println(parsedText);
+        }
 
         return parsedText;
     }
