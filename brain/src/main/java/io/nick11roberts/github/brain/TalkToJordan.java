124fd8176ca04eca7ba32c5ad34a5f06ac28e1ee
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2015-07-17 23:56:19 -0700

    Jordan is now completely in memory. Jordan will be redeployed, even though Jordan is now prone to forgetfulness... This will change soon.

--- brain/src/main/java/io/nick11roberts/github/brain/TalkToJordan.java
+++ brain/src/main/java/io/nick11roberts/github/brain/TalkToJordan.java
@@ -3,45 +3,63 @@ package io.nick11roberts.github.brain;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.Query;
 
-import org.jgrapht.ext.DOTExporter;
-import org.jgrapht.ext.EdgeNameProvider;
-import org.jgrapht.ext.IntegerEdgeNameProvider;
-import org.jgrapht.ext.StringNameProvider;
 import org.jgrapht.graph.DefaultDirectedWeightedGraph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 
-import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
-import java.util.Stack;
 
 import javax.inject.Named;
-
-import static io.nick11roberts.github.brain.OfyService.ofy;
 
 @Api(name = "talkToJordan", version = "v1", namespace = @ApiNamespace(ownerDomain = "brain.github.nick11roberts.io", ownerName = "brain.github.nick11roberts.io", packagePath = ""))
 public class TalkToJordan {
 
     private DefaultDirectedWeightedGraph<String, DefaultWeightedEdge> wordNetwork = new DefaultDirectedWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
 
+    private final int RESPONSE_LENGTH_VARIATION = 3;
+
+    /*
+    @ApiMethod(name = "getState")
+    public ResponseStatement getState(){
+        StringNameProvider<String> vertexLabel = new StringNameProvider<String>(){
+            public String getVertexName(String p) {
+                return p;
+            }
+        };
+
+        EdgeNameProvider<Integer> edgeLabel = new IntegerEdgeNameProvider<Integer>();
+
+        DOTExporter<String, DefaultWeightedEdge> exporter =
+                new DOTExporter<>(vertexLabel, null, null, null, null); //Use a different constructor
+        StringWriter w = new StringWriter();
+        exporter.export(w, wordNetwork);
+
+        ResponseStatement responseWrapper = new ResponseStatement(w.toString());
+
+        System.out.println(responseWrapper.getStatement());
+
+        //Doesn't work properly. Ideally, this should generate a text file with these contents.
+        return responseWrapper;
+    }
+    */
 
     @ApiMethod(name = "prompt")
     public ResponseStatement prompt(@Named("inputStatement") String inputStatement) {
+        int responseMaxSize = 0;
         ResponseStatement response = new ResponseStatement("Hey");
         TrainingExampleManager tem = new TrainingExampleManager();
         List<List<String>> parsedText = tem.parseText(inputStatement);
         for (int i = 0; i <= parsedText.size() - 1; i++){ // for number of sentences
             for (int j = 0; j <= parsedText.get(i).size() - 1; j++){ // for number of words per sentence
-                //add current vertex to the graph
+                //Add current vertex to the graph
                 wordNetwork.addVertex(parsedText.get(i).get(j));
+                //Recalculate response max size if needed
+                if(parsedText.get(i).size() > responseMaxSize){
+                    responseMaxSize = parsedText.get(i).size();
+                }
                 if (j >= 1) {
                     if (!wordNetwork.containsEdge(parsedText.get(i).get(j - 1), parsedText.get(i).get(j))){
                         //Add edge to graph
@@ -66,84 +84,67 @@ public class TalkToJordan {
             }
         }
 
-        StringNameProvider<String> vertexLabel = new StringNameProvider<String>(){
-            public String getVertexName(String p) {
-                return p;
-            }
-        };
-
-        EdgeNameProvider<Integer> edgeLabel = new IntegerEdgeNameProvider<Integer>();
-
-
-
-        //ResponseStatement response = new ResponseStatement(generateResponse(10));
-        DOTExporter<String, DefaultWeightedEdge> exporter =
-                new DOTExporter<>(vertexLabel, null, null, null, null); //Use a different constructor
-        StringWriter w = new StringWriter();
-        exporter.export(w, wordNetwork);
-        System.out.println(w.toString());
-
         //Response should be a random walk, so write a random walk function
+        response.setStatement(
+                randomWalkForResponse(
+                        randInt(
+                                responseMaxSize/RESPONSE_LENGTH_VARIATION,
+                                responseMaxSize*RESPONSE_LENGTH_VARIATION
+                        )
+                )
+        );
+
         return response;
     }
 
-    private Edge findEdgeRecord(Vertex vertexFrom, Vertex vertexTo) {
-        String searchableVertexTuple = "("
-                + vertexFrom.getWord()
-                + ", "
-                + vertexTo.getWord()
-                + ")";
-
-        return ofy().load().type(Edge.class).id(searchableVertexTuple).now();
-    }
-
-    private String generateResponse(int inputStatementLength){
-        Map<Integer, String> punctuation = new HashMap<>();
-        punctuation.put(0,".");
-        punctuation.put(1,"!");
-        punctuation.put(2,"?");
+    private String randomWalkForResponse(int maxSize){
+        //The response can be up to maxSize long
         String response = "";
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        Stack<List<Entity>> edgeQueryStack = new Stack<List<Entity>>();
-        Stack<Vertex> vertexStack = new Stack<Vertex>();
 
-        Query q = new Query("Vertex");
-        List<Entity> results = datastore.prepare(q).asList(FetchOptions.Builder.withDefaults());
+        //Generate random punctuation
+        Map<Integer, String> punctuationMap = new HashMap<>();
+        punctuationMap.put(0,".");
+        punctuationMap.put(1,"!");
+        punctuationMap.put(2,"?");
+        String punctuation = punctuationMap.get(randInt(0, punctuationMap.size() - 1));
 
-        Integer randomInitialWordIndex = randInt(0, results.size() - 1);
-        com.google.appengine.api.datastore.Key currentVertexKey = results.get(randomInitialWordIndex).getKey();
+        //Get a random vertex from the wordNetwork graph
+        int randomVertexIndex = randInt(0, wordNetwork.vertexSet().size() - 1);
+        String currentRandomVertex = wordNetwork.vertexSet().toArray()[randomVertexIndex].toString();
 
-        String currentWord = results.get(randomInitialWordIndex).getKey().getName();
-        response += currentWord;
+        //Initial update of response
+        response += currentRandomVertex;
 
-        for(int i = 0; i <= inputStatementLength-1; i++){
-            //find list of connecting edges
-            edgeQueryStack.push(
-                    datastore.prepare(
-                            new Query("Edge").setFilter(
-                                    new Query.FilterPredicate(
-                                            "vertexFromWord", Query.FilterOperator.EQUAL, currentWord
-                                    )
-                            )
-                    ).asList(FetchOptions.Builder.withDefaults())
-            );
+        //Walk
+        for (int i = 0; i <= maxSize-2; i++){
+            //Choose a new vertex if possible
+            if (wordNetwork.outDegreeOf(currentRandomVertex) > 0) {
 
-            if(!edgeQueryStack.isEmpty() && edgeQueryStack.peek().size()>0) {
-                //choose random edge, get next vertex, reset current word
-                currentWord = (String)edgeQueryStack.peek().get(
-                                randInt(0, edgeQueryStack.peek().size() - 1)
-                        ).getProperty("vertexToWord");
-                //append current word
-                response += " " + currentWord;
+                //Space out each word
+                response += " ";
+
+                //Find new random vertex
+                currentRandomVertex = wordNetwork
+                        .getEdgeTarget(
+                                (DefaultWeightedEdge) wordNetwork
+                                        .outgoingEdgesOf(currentRandomVertex)
+                                        .toArray()[randInt(0, wordNetwork.outDegreeOf(currentRandomVertex) - 1)]
+                        );
+
+                //Update response
+                response += currentRandomVertex;
+            } else {
+
+                //Add punctuation and leave the loop, we have reached a leaf
+                response+=punctuation;
+                break;
+
             }
-            edgeQueryStack.pop();
         }
-        response += punctuation.get(randInt(0, punctuation.size()-1));
-        response = response.substring(0, 1).toUpperCase() + response.substring(1);
         return response;
     }
 
-    public static int randInt(int min, int max) {
+    private static int randInt(int min, int max) {
 
         // NOTE: Usually this should be a field rather than a method
         // variable so that it is not re-seeded every call.
